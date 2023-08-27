676e2041482399eea32ce5e1f512c0e1fabbb489
rakeem <rakeemshaikh4545@gmail.com>
2023-08-27 23:22:24 +0530

    valid palindrome

---
+++ String/Valid_Palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool isPalindrome(string s) {
+        
+
+        int i=0;
+        int j=s.length()-1;
+
+        while(i <= j)
+        {
+            if(!isalnum(s[i]))
+            {
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j]))
+            {
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])) return 0;
+            else{
+                i++;
+                j--;
+            }
+
+        }
+
+        return 1;
+
+    }
+};
