891a51521c1e5907ac009ed42749e3358a51cba6
rakeem <rakeemshaikh4545@gmail.com>
2023-09-19 15:41:21 +0530

    maximum-nesting-depth-of-the-parentheses

---
+++ String/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        
+
+        int n = s.size();
+        int count = 0;
+        int max = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == '(')
+            {
+                count++;
+            }
+            else if(s[i] == ')')
+            {
+                count--;
+            }
+
+            if(count > max) max = count;
+        }
+
+        return max;
+    }
+};
