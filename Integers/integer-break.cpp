caf426938fdb14fdc41684e2ebd76199feee7988
Rakeem Shaikh <rakeemshaikh4545@gmail.com>
2023-10-06 23:38:34 +0530

    integer break

---
+++ Integers/integer-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        
+        if(n==2 || n==3) return n-1;
+
+        int ans = 1;
+
+        while(n > 4)
+        {
+            n = n-3;
+            ans = ans*3;
+        }
+
+        return ans*n;
+    }
+};
