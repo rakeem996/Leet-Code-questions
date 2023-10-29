b24c33e288fa6054f851a7384c9e41e609804289
Rakeem Shaikh <rakeemshaikh4545@gmail.com>
2023-10-29 22:52:51 +0530

    swap nodes in pairs

---
+++ Recursion/swap-nodes-in-pairs.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+        if(!head || !head->next) return head;
+
+        ListNode* temp = head->next;
+        head->next = head->next->next;
+        temp->next = head;
+        head = temp;
+
+        ListNode* nextHead = swapPairs(head->next->next);
+        head->next->next = nextHead;
+
+        return head;
+    }
+};
